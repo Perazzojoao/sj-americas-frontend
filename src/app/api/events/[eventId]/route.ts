ef87554b2cbb4ea075677979a6e030fb09736046
Perazzojoao <perazzojoao@gmail.com>
2025-01-12 17:31:57 -0300

    refatorar tratamento de resposta no método PATCH para eventos e adicionar verificação de erro

--- src/app/api/events/[eventId]/route.ts
+++ src/app/api/events/[eventId]/route.ts
@@ -2,23 +2,34 @@ import { BASE_URL } from '@/services/baseUrl'
 import { NextRequest, NextResponse } from 'next/server'
 import { revalidateTag } from 'next/cache'
 import { eventFormZodType } from '@/components/Events/EventList/Event/EventForm/eventFormZod'
+import { stat } from 'fs'
 
 export async function PATCH(request: NextRequest, { params }: { params: Promise<{ eventId: string }> }) {
-	try {
-		const { eventId } = await params
-		const body: eventFormZodType = await request.json()
-		const response = await fetch(`${BASE_URL}/event/${eventId}`, {
-			method: 'PATCH',
-			body: JSON.stringify(body),
-			headers: {
-				'Content-Type': 'application/json',
+	const { eventId } = await params
+	const body: eventFormZodType = await request.json()
+	const response = await fetch(`${BASE_URL}/event/${eventId}`, {
+		method: 'PATCH',
+		body: JSON.stringify(body),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+
+	if (!response.ok) {
+		const body = await response.json()
+		const statusCode = response.status
+		return NextResponse.json(
+			{
+				message: body.message,
+				error: body.error,
+				status: statusCode,
 			},
-		})
-
-		revalidateTag('event')
-
-		return NextResponse.json(response)
-	} catch (error) {
-		throw new Error(String(error))
+			{
+				status: statusCode,
+			}
+		)
 	}
+	revalidateTag('event')
+
+	return NextResponse.json(response)
 }
