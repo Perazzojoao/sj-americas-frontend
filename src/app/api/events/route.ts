e8064505f12d0ac11d12b2c0feff7af2fcb7c8c9
Perazzojoao <perazzojoao@gmail.com>
2025-01-12 20:12:14 -0300

    adicionar suporte para criação de eventos com método POST e tratamento de erros

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,7 +1,39 @@
-import { getEventList } from '@/services';
-import { NextResponse } from 'next/server'
+import { eventFormZodType } from '@/components/Events/EventList/Event/EventForm/eventFormZod'
+import { getEventList } from '@/services'
+import { BASE_URL } from '@/services/baseUrl'
+import { revalidateTag } from 'next/cache'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET() {
-  const response = await getEventList();
-  return NextResponse.json(response);
+	const response = await getEventList()
+	return NextResponse.json(response)
+}
+
+export async function POST(request: NextRequest) {
+	const body: eventFormZodType = await request.json()
+	const response = await fetch(`${BASE_URL}/event`, {
+		method: 'POST',
+		body: JSON.stringify(body),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+
+	if (!response.ok) {
+		const body = await response.json()
+		const statusCode = response.status
+		return NextResponse.json(
+			{
+				message: body.message,
+				error: body.error,
+				status: statusCode,
+			},
+			{
+				status: statusCode,
+			}
+		)
+	}
+	revalidateTag('event')
+
+	return NextResponse.json(response)
 }
