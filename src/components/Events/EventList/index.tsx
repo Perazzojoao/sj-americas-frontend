1b10338a270dade31bc841be3fa8100ca03f8a70
Perazzojoao <perazzojoao@gmail.com>
2025-01-13 00:29:13 -0300

    adicionar diálogo para criação de eventos e funcionalidade de busca na lista de eventos

--- src/components/Events/EventList/index.tsx
+++ src/components/Events/EventList/index.tsx
@@ -1,17 +1,68 @@
 'use client'
+import { Button } from "@/components/ui/button";
 import Event from "./Event";
 import useEventList from "@/hooks/useEventList";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import CreateEventForm from "./Event/EventForm/createEventForm";
+import { Input } from "@/components/ui/input";
+import { FaPlus } from "react-icons/fa6";
+import { useState } from "react";
+
+const CreateEventDialog = () => {
+  return (
+    <Dialog >
+      <DialogTrigger asChild className="flex justify-between items-center">
+        <Button className="bg-primary text-white max-w-72 grow">
+          Criar evento
+          <FaPlus className="ml-2" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px] max-w-[360px] rounded-lg bg-white">
+        <DialogHeader>
+          <DialogTitle className="text-primary">Criar evento</DialogTitle>
+        </DialogHeader>
+
+        <CreateEventForm />
+
+      </DialogContent>
+    </Dialog>
+  )
+}
 
 const EventList = () => {
   const { eventList } = useEventList();
+  const [search, setSearch] = useState("");
+
+  function handleSearchChange(text: string) {
+    setSearch(text);
+  }
 
   return (
     <div>
-      <h1 className="text-2xl font-semibold text-violet-700 mb-3">Lista de eventos</h1>
+      <div className="w-full flex justify-center items-center gap-4 mb-6">
+        <Input
+          className="bg-white ring-white border-white w-3/5 sm:w-3/6"
+          placeholder="Buscar evento"
+          onChange={(e) => handleSearchChange(e.target.value)}
+        />
+      </div>
+      <section className="w-full flex justify-between gap-5 items-center mb-5">
+        <h1 className="text-2xl font-semibold text-primary">Lista de eventos</h1>
+        <CreateEventDialog />
+      </section>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {eventList.map((event) => (
-          <Event key={event.id} {...event} />
-        ))}
+        {eventList
+          .filter((event) => event.name.toLowerCase().includes(search.toLowerCase()))
+          .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+          .map((event) => (
+            <Event key={event.id} {...event} />
+          ))}
       </div>
     </div>
   );
