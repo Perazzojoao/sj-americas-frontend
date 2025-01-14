cdb621c61dc34ec60e99412f8524a293eb927bd2
Perazzojoao <perazzojoao@gmail.com>
2025-01-14 02:09:08 -0300

    adicionar novos componentes de diálogo e dropdown ao evento, incluindo funcionalidades de edição e exclusão

--- src/components/Events/EventList/Event/index.tsx
+++ src/components/Events/EventList/Event/index.tsx
@@ -13,8 +13,27 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+
 import EventForm from "./EventForm/updateEventForm";
+import { MoreHorizontal } from "lucide-react";
 
 const Event = (event: event) => {
   const { name, date, tableCount } = event;
@@ -33,9 +52,38 @@ const Event = (event: event) => {
           Mesas: <span className="font-normal text-muted-foreground">{tableCount}</span>
         </div>
         <Dialog>
-          <DialogTrigger asChild>
-            <Button className="bg-primary">Editar</Button>
-          </DialogTrigger>
+          <AlertDialog>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <MoreHorizontal className="bg-background rounded-full p-1 cursor-pointer hover:animate-jump animate-once animate-duration-[800ms] animate-delay-200 animate-ease-out" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent side="right" align="start" >
+                <DialogTrigger asChild>
+                  <DropdownMenuItem className="hover:cursor-pointer">
+                    <span className="text-primary font-semibold">Editar</span>
+                  </DropdownMenuItem>
+                </DialogTrigger>
+                <AlertDialogTrigger asChild>
+                  <DropdownMenuItem className="hover:cursor-pointer">
+                    <span className="text-primary font-semibold">Deletar</span>
+                  </DropdownMenuItem>
+                </AlertDialogTrigger>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <AlertDialogContent className="bg-white">
+              <AlertDialogHeader>
+                <AlertDialogTitle className="text-primary">Você tem certeza?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  Essa ação não pode ser desfeita. Isso excluirá permanentemente esse
+                  evento e todos os dados relacionados.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                <AlertDialogAction>Deletar</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
           <DialogContent className="sm:max-w-[425px] max-w-[360px] rounded-lg bg-white">
             <DialogHeader>
               <DialogTitle className="text-primary">Editar evento</DialogTitle>
