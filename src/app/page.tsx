3f226326c5ec9713acf1a8acdffde04c782d1c4d
Perazzojoao <perazzojoao@gmail.com>
2025-01-14 20:21:57 -0300

    adicionar componente Toaster ao layout e remover do componente Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Header from "@/components/Header";
 import Events from "@/components/Events";
-import { Toaster } from "@/components/ui/toaster"
 
 export default function Home() {
   return (
     <main>
       <Header></Header>
       <div className="px-3 sm:px-5 sm:w-4/5 md:w-4/6 mx-auto mb-8">
-        <Toaster />
         <Events />
       </div>
     </main>
