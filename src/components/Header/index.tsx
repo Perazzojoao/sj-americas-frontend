339bad6fd7e76ec36588abbfd1637b16765d2986
Perazzojoao <perazzojoao@gmail.com>
2025-01-15 15:41:35 -0300

    atualizar estilos do cabeçalho para usar cores secundárias e de fundo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex justify-start items-center bg-gray-300 p-3 sm:p-5 mb-5 z-50 sticky top-0 w-full shadow-md">
+    <header className="flex justify-start items-center bg-secondary p-3 sm:p-5 mb-5 z-50 sticky top-0 w-full shadow-md">
       <Link href="/">
-        <h1 className="uppercase text-primary font-bold text-3xl sm:text-4xl">eventos</h1>
+        <h1 className="uppercase text-background font-bold text-3xl sm:text-4xl">eventos</h1>
       </Link>
     </header>
   );
