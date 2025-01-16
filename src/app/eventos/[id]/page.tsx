9cbd012333a397ca1207149b158cbe7a11c38c9e
Perazzojoao <perazzojoao@gmail.com>
2025-01-15 23:02:52 -0300

    remover a chamada de getTableList e integrar o hook useTableList no componente Tables

--- src/app/eventos/[id]/page.tsx
+++ src/app/eventos/[id]/page.tsx
@@ -1,5 +1,4 @@
 import Tables from "@/components/Tables";
-import { getTableList } from "@/services";
 
 type EventPageProps = {
   params: Promise<{
@@ -9,10 +8,9 @@ type EventPageProps = {
 
 const EventPage = async ({ params }: EventPageProps) => {
   const { id } = await params;
-  const tableList = await getTableList(parseInt(id));
   return (
     <div className="px-3 sm:px-4 lg:px-5 lg:w-4/5 xl:w-4/6 mx-auto mb-8">
-      <Tables eventId={parseInt(id)} tableList={tableList} />
+      <Tables eventId={parseInt(id)} />
     </div>
   );
 };
--- src/components/Tables/index.tsx
+++ src/components/Tables/index.tsx
@@ -1,13 +1,15 @@
+'use client'
 import { table } from "@/@types";
 import { DataTable } from "./DataTable/data-table";
 import { columns } from "./DataTable/columns";
+import useTableList from "@/hooks/useTableList";
 
 type TablesProps = {
   eventId: number;
-  tableList: table[]
 }
 
-const Tables = ({ eventId, tableList }: TablesProps) => {
+const Tables = ({ eventId }: TablesProps) => {
+  const { tableList } = useTableList(eventId);
   return (
     <div>
       <DataTable columns={columns} data={tableList} />
