3a09513ba244fb9e5742b24540365d295c4c7467
Perazzojoao <perazzojoao@gmail.com>
2025-01-15 23:03:30 -0300

    renomear hook useTablesList para useTableList e alterar a função de busca para utilizar a API

--- src/hooks/useTableList.ts
+++ src/hooks/useTableList.ts
@@ -1,11 +1,12 @@
 import { table } from "@/@types";
 import { getTableList } from "@/services";
+import { NEXT_API_URL } from "@/services/baseUrl";
 import { useQuery } from "@tanstack/react-query";
 
-export default function useTablesList(eventId: number | undefined = undefined) {
+export default function useTableList(eventId: number | undefined = undefined) {
   const { data, error, isLoading, refetch } = useQuery<table[]>({
     queryKey: ["tableList", eventId],
-    queryFn: () => getTableList(eventId),
+    queryFn: () => fetch(`${NEXT_API_URL}/api/tables/${eventId}`).then((res) => res.json()),
   })
 
   const tableList = data ?? [];
