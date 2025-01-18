c2a2741ad389dde8a95da0f3f40cb3b67cd1dcdb
Perazzojoao <perazzojoao@gmail.com>
2025-01-18 17:22:26 -0300

    adicionar endpoint PATCH para atualização de múltiplas mesas

---
+++ src/app/api/tables/multiples/route.ts
@@ -0,0 +1,40 @@
+import { tableFormSchemaType } from "@/components/Tables/TableForm/tableFormSchema"
+import { BASE_URL } from "@/services/baseUrl"
+import { revalidateTag } from "next/cache"
+import { NextRequest, NextResponse } from "next/server"
+
+type BodyType = {
+data: tableFormSchemaType,
+table_list_ids: number[]
+}
+
+export async function PATCH(request: NextRequest) {
+	const { data, table_list_ids }: BodyType = await request.json()
+  const formatedBody = {...data, table_list_ids}
+  console.log('formatedBody:', formatedBody);
+	const response = await fetch(`${BASE_URL}/table/multiples`, {
+		method: 'PATCH',
+		body: JSON.stringify(formatedBody),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+
+	if (!response.ok) {
+		const body = await response.json()
+		const statusCode = response.status
+		return NextResponse.json(
+			{
+				message: body.message,
+				error: body.error,
+				status: statusCode,
+			},
+			{
+				status: statusCode,
+			}
+		)
+	}
+	revalidateTag('table')
+
+	return NextResponse.json(response)
+}
