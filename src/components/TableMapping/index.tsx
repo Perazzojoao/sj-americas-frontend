bc02c8c0ac3868776c3f57f6d590eeefd6a8f88e
Perazzojoao <perazzojoao@gmail.com>
2025-01-23 00:53:39 -0300

    adicionar lógica para dividir mesas pequenas em duas metades no componente TableMapping

--- src/components/TableMapping/index.tsx
+++ src/components/TableMapping/index.tsx
@@ -8,10 +8,13 @@ type TablesProps = {
 }
 
 const TableMapping = ({ tableList }: TablesProps) => {
-  const { lgTableList, smTableList } = useMemo(() => {
+  const { lgTableList, smTableList, firstHalfSmTables, secondHalfSmTables } = useMemo(() => {
     const lgTableList = tableList.filter((table) => table.seats > 4);
     const smTableList = tableList.filter((table) => table.seats <= 4);
-    return { lgTableList, smTableList };
+    const halfLength = Math.ceil(smTableList.length / 2);
+    const firstHalfSmTables = smTableList.slice(0, halfLength);
+    const secondHalfSmTables = smTableList.slice(halfLength);
+    return { lgTableList, smTableList, firstHalfSmTables, secondHalfSmTables };
   }, [tableList]);
 
   return (
@@ -22,16 +25,16 @@ const TableMapping = ({ tableList }: TablesProps) => {
         <h3 className="text-center border-b-2 border-primary text-xs sm:text-lg pb-2 mb-2 sm:mb-0">{lgTableList[0]?.seats} Cadeiras</h3>
         <div className="flex flex-col justify-between items-center gap-[66px] sm:gap-[104px] col-start-1 row-start-2 col-end-1 row-end-2">
           <div className="grid grid-rows-2 auto-cols-max grid-flow-col gap-2 sm:gap-4 w-fit">
-            {smTableList.slice(0, 5).map((table, index) => (
+            {firstHalfSmTables.map((table, index) => (
               <div key={index} className={`${table.number === 1 ? 'row-span-2' : ''}`}>
-                <Table table={table} />
+                <Table key={table.id} table={table} />
               </div>
             ))}
           </div>
           <div className="grid grid-rows-2 auto-cols-max grid-flow-col gap-2 sm:gap-4 w-fit">
-            {smTableList.slice(5).map((table, index) => (
+            {secondHalfSmTables.map((table, index) => (
               <div key={index} className={`${table.number === 6 ? 'row-span-2 row-start-2 row-end-2' : ''}`}>
-                <Table table={table} />
+                <Table key={table.id} table={table} />
               </div>
             ))}
           </div>
@@ -39,17 +42,17 @@ const TableMapping = ({ tableList }: TablesProps) => {
         <div className="flex flex-col gap-2 sm:gap-4 items-center col-start-2 row-start-2 col-end-2 row-end-2">
           <div className="grid grid-rows-2 auto-cols-max grid-flow-col gap-2 sm:gap-4 w-fit">
             {lgTableList.slice(0, 20).map((table) => (
-              <Table table={table} />
+              <Table key={table.id} table={table} />
             ))}
           </div>
           <div className="grid grid-rows-2 auto-cols-max grid-flow-col gap-2 sm:gap-4 w-fit ml-7 sm:ml-11">
             {lgTableList.slice(20, 38).map((table) => (
-              <Table table={table} />
+              <Table key={table.id} table={table} />
             ))}
           </div>
           <div className="grid grid-rows-2 auto-cols-max grid-flow-col gap-2 sm:gap-4 w-fit">
             {lgTableList.slice(38).map((table) => (
-              <Table table={table} />
+              <Table key={table.id} table={table} />
             ))}
           </div>
         </div>
