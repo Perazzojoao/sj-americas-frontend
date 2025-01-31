497066c5120c73259f8ddb1381a3244489c8343a
Perazzojoao <perazzojoao@gmail.com>
2025-01-31 13:27:23 -0300

    atualizar estrutura do componente de login para utilizar uma seção e ajustar o estilo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,11 @@
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from "@/components/LoginForm"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <section className="flex min-h-96 w-full items-center justify-center p-3 md:p-10 mt-24">
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
-    </div>
+    </section>
   )
 }
