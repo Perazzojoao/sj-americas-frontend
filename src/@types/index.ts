9004245ee541798e3c3b5e3e55817c2ffe4522e8
Perazzojoao <perazzojoao@gmail.com>
2025-01-31 13:28:05 -0300

    remover componentes de login e atualizar tipos de resposta para incluir dados de login

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,44 +1,68 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react'
 
 export type response = {
-  statusCode: number;
-  message: string;
-  data: eventListResponse | eventResponse | tableListResponse | tableResponse;
+	statusCode: number
+	message: string
+	data: eventListResponse | eventResponse | tableListResponse | tableResponse
 }
 
 export type eventListResponse = {
-  event_list: event[];
+	event_list: event[]
 }
 
 export type eventResponse = {
-  event: event;
+	event: event
 }
 
 export type tableListResponse = {
-  tables: table[];
+	tables: table[]
 }
 
 export type tableResponse = {
-  table: table;
+	table: table
 }
 
 export type event = {
-  id: number;
-  name: string;
-  date: string;
-  tableCount: number;
-  createdAt: string;
-  updatedAt: string;
+	id: number
+	name: string
+	date: string
+	tableCount: number
+	createdAt: string
+	updatedAt: string
 }
 
 export type table = {
-  id: number;
-  number: number;
-  seats: number;
-  owner: number | null;
-  eventId: number;
-  isTaken: boolean;
-  isPaid: boolean;
-  createdAt: string;
-  updatedAt: string;
+	id: number
+	number: number
+	seats: number
+	owner: number | null
+	eventId: number
+	isTaken: boolean
+	isPaid: boolean
+	createdAt: string
+	updatedAt: string
+}
+
+export type user = {
+	id: number
+	user_name: string
+	role: Role
+	createdAt: string
+	updatedAt: string
+}
+
+export enum Role {
+	ADMIN,
+	USER,
+}
+
+export type LoginData = {
+	token: string
+	user: user
+}
+
+export type LoginResponse = {
+	statusCode: number
+	message: string
+	data: LoginData
 }
