462cbe76701bcda661f928b14087d945bff1d593
Perazzojoao <perazzojoao@gmail.com>
2025-02-03 14:00:39 -0300

    adicionar rotas para obter e criar usuários com autenticação via token

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,41 @@
+import { getBearerToken } from '@/lib/getBearer'
+import { getUserList } from '@/services'
+import { BASE_URL } from '@/services/baseUrl'
+import { revalidateTag } from 'next/cache'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET() {
+	const response = await getUserList()
+	return NextResponse.json(response)
+}
+
+export async function POST(request: NextRequest) {
+	const bearerToken = await getBearerToken()
+	const body = await request.json()
+	const response = await fetch(`${BASE_URL}/users`, {
+		method: 'POST',
+		body: JSON.stringify(body),
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: bearerToken,
+		},
+	})
+
+	if (!response.ok) {
+		const body = await response.json()
+		const statusCode = response.status
+		return NextResponse.json(
+			{
+				message: body.message,
+				error: body.error,
+				status: statusCode,
+			},
+			{
+				status: statusCode,
+			}
+		)
+	}
+	revalidateTag('user')
+
+	return NextResponse.json(response)
+}
