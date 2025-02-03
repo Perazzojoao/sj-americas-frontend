3812de4ae07e9dc061c07a30db6623db81a6c9d6
Perazzojoao <perazzojoao@gmail.com>
2025-02-03 14:21:59 -0300

    ajustar espaçamento inferior do componente SearchInput

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -7,7 +7,7 @@ type SerachInputProps = {
 
 const SearchInput = ({ handleSearchChange, placeHolder }: SerachInputProps) => {
   return (
-    <div className="w-full flex justify-center items-center gap-4 mb-6">
+    <div className="w-full flex justify-center items-center gap-4 mb-8">
       <Input
         className="bg-card w-3/5 sm:w-3/6"
         placeholder={placeHolder}
