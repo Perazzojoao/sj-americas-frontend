02087847f737905fde2ec33acbe8a0ab240f2894
Perazzojoao <perazzojoao@gmail.com>
2025-02-03 15:22:11 -0300

    adicionar tabela de usuários com seleção, filtragem e paginação

---
+++ src/components/Users/DataTable/columns.tsx
@@ -0,0 +1,136 @@
+'use client'
+import { user } from "@/@types"
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Checkbox } from "@/components/ui/checkbox"
+import { useSelectedItems } from "@/hooks/useSelectedItems"
+import { CheckedState } from "@radix-ui/react-checkbox"
+
+export const columns: ColumnDef<user>[] = [
+  {
+    id: "select",
+    header: ({ table }) => {
+      const { handleAllCheckboxChange } = useSelectedItems()
+
+      function handleCheckboxChange(value: CheckedState) {
+        table.toggleAllPageRowsSelected(!!value)
+        handleAllCheckboxChange(!!value)
+      }
+
+      return (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => handleCheckboxChange(value)}
+          aria-label="Select all"
+        />
+      )
+    },
+    cell: ({ row }) => {
+      function handleCheckboxChange(value: CheckedState) {
+        row.toggleSelected(!!value)
+      }
+
+      return (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => handleCheckboxChange(value)}
+          aria-label="Select row"
+        />
+      )
+    },
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "user_name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Nome de usuário
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const user_name: string = row.getValue("user_name")
+      return <div className="text-left pl-4">{user_name}</div>
+    },
+  },
+  {
+    accessorKey: "role",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting()}
+        >
+          Função
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const role: string = row.getValue("role")
+      return <div className="text-left pl-4">{role}</div>
+    },
+  },
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Data de criação
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const createdAt: string = row.getValue("createdAt")
+      const formattedDate = new Date(createdAt).toLocaleDateString('pt-BR')
+      return <div className="text-left pl-4">{formattedDate}</div>
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const user = row.original
+
+      return (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              size="sm"
+              className="flex items-center"
+              aria-label="Actions"
+            >
+              Editar
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px] max-w-[360px] rounded-lg bg-card">
+            <DialogHeader>
+              <DialogTitle className="text-primary">Editar usuário: {user.user_name}</DialogTitle>
+            </DialogHeader>
+            {/* <UpdateTableForm {...user} /> */}
+          </DialogContent>
+        </Dialog>
+      )
+    },
+  }
+]
---
+++ src/components/Users/DataTable/data-table.tsx
@@ -0,0 +1,152 @@
+"use client"
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getPaginationRowModel,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import { useSelectedItems } from "@/hooks/useSelectedItems"
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { ArrowRightToLine, ArrowLeftToLine } from "lucide-react"
+
+type DataTableProps<TData, TValue> = {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+    },
+  })
+
+  return (
+    <section>
+      <div className="flex items-center py-4 gap-3">
+        <Input
+          placeholder="Filtrar usuários"
+          value={(table.getColumn("user_name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("user_name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm bg-card"
+        />
+      </div>
+      <div className="rounded-md border">
+        <Table className="bg-card rounded-lg">
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  Nenhum resultado.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} de{" "}
+          {table.getFilteredRowModel().rows.length} íten(s) selecionados.
+        </div>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.firstPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          <ArrowLeftToLine className="h-4 w-4" />
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.lastPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          <ArrowRightToLine className="h-4 w-4" />
+        </Button>
+      </div>
+    </section>
+  )
+}
