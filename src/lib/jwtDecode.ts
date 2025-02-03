2df8b61589995845455fed092ca154415383b203
Perazzojoao <perazzojoao@gmail.com>
2025-02-03 16:19:27 -0300

    atualizar interface JWTPayload para incluir o papel 'SUPER_ADMIN' e melhorar a formatação do código no jwtDecodeToken

--- src/lib/jwtDecode.ts
+++ src/lib/jwtDecode.ts
@@ -1,29 +1,27 @@
-'use server'
-
 import { jwtDecode } from 'jwt-decode'
 
 interface JWTPayload {
-  exp: number
-  role: 'ADMIN' | 'USER'
-  [key: string]: any
+	exp: number
+	role: 'SUPER_ADMIN' | 'ADMIN' | 'USER'
+	[key: string]: any
 }
 
 export function jwtDecodeToken(token: string) {
-  // Verificar se o token está no formato correto (três partes separadas por ponto)
-  if (!token || !token.includes('.') || token.split('.').length !== 3) {
-    return { valid: false, expired: false, payload: null }
-  }
+	// Verificar se o token está no formato correto (três partes separadas por ponto)
+	if (!token || !token.includes('.') || token.split('.').length !== 3) {
+		return { valid: false, expired: false, payload: null }
+	}
 
-  try {
-    const payload = jwtDecode<JWTPayload>(token)
-    const now = Date.now() / 1000
+	try {
+		const payload = jwtDecode<JWTPayload>(token)
+		const now = Date.now() / 1000
 
-    if (payload.exp && payload.exp < now) {
-      return { valid: false, expired: true, payload: null }
-    }
+		if (payload.exp && payload.exp < now) {
+			return { valid: false, expired: true, payload: null }
+		}
 
-    return { valid: true, expired: false, payload }
-  } catch (error) {
-    return { valid: false, expired: false, payload: null }
-  }
+		return { valid: true, expired: false, payload }
+	} catch (error) {
+		return { valid: false, expired: false, payload: null }
+	}
 }
