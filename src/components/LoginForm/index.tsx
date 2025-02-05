4163785484cfda9cc85686680cbc3a7b6432170e
Perazzojoao <perazzojoao@gmail.com>
2025-02-05 13:59:08 -0300

    adicionar atualização de rota após login para usuários com papéis ADMIN e SUPER_ADMIN

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -56,6 +56,7 @@ export function LoginForm({
       const responseBody: LoginResponse = await response.json()
       const user = responseBody.data.user
       const userRole = user.role
+      route.refresh()
       if (userRole === Role.ADMIN || userRole === Role.SUPER_ADMIN) {
         route.push('/admin')
         return;
